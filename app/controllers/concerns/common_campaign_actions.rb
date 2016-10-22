5ad96f72ec40c10cb341eef91fc962d5325d32fa
Scott Stevenson <sstevenson@credera.com>
2016-10-22 15:22:36 -0500

    merging

--- app/controllers/concerns/common_campaign_actions.rb
+++ app/controllers/concerns/common_campaign_actions.rb
@@ -2,7 +2,7 @@ module CommonCampaignActions
   extend ActiveSupport::Concern
 
   def index
-    @campaigns = current_organization.campaigns
+    @campaigns = @organization.campaigns
     respond_to do |format|
       format.html
       format.json { render json: @campaigns }
@@ -10,7 +10,7 @@ module CommonCampaignActions
   end
 
   def show
-    @campaign = current_organization.campaigns.find(params[:id])
+    @campaign = @organization.campaigns.find(params[:id])
     respond_to do |format|
       format.html
       format.json { render json: @campaign }
@@ -24,5 +24,5 @@ module CommonCampaignActions
       format.json { render json: @campaign }
     end
   end
-  
+
 end
