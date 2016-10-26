af5652181ec8bdc68eeaf0f067b2f31162e88a1c
Scott Stevenson <sstevenson@credera.com>
2016-10-26 09:25:30 -0500

    UI cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def admin?
+    controller.class.parent == Admin
+  end
+
+  def partner?
+    controller.class.parent == Partner
+  end
 end
--- app/models/catalog_entry.rb
+++ app/models/catalog_entry.rb
@@ -1,4 +1,5 @@
 class CatalogEntry < ApplicationRecord
   belongs_to :catalog
   belongs_to :book
+  has_many :wishlist_entries
 end
