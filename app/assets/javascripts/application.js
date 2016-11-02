f192e24fb499a3d4cb85f4ab2c93f8a1d9205451
Scott Stevenson <sstevenson@credera.com>
2016-11-02 08:50:25 -0500

    Fixing javascript error with wishlist edit. Hard coding donation amount to 30 Implementing oncomplete for kimbia form. Not working yet...need Kimbia input

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+// require_tree .
 //= require bootstrap-sprockets
 //= require moment
 //= require bootstrap-datetimepicker
