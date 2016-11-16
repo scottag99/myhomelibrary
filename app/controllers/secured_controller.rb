700d54e905391473de47f8ccfa37cd78b79db9ba
Scott Stevenson <sstevenson@credera.com>
2016-11-16 13:11:49 -0600

    Used wrong method...should have been `blank?`

--- app/controllers/secured_controller.rb
+++ app/controllers/secured_controller.rb
@@ -13,19 +13,18 @@ class SecuredController < ApplicationController
   end
 
   def has_role?
-    role = ''
     if session[:userinfo]['extra'] && session[:userinfo]['extra']['raw_info']
       role = session[:userinfo]['extra']['raw_info']['role']
     end
 
     # if this isn't an admin user, check to see if they are assigned as a partner for an organization
-    if role.empty?
+    if role.blank?
       if Organization.joins(:partners).where('partners.email = ? and partners.active = ?', session[:userinfo]['info']['email'], true).count() > 0
         role = 'partner'
       end
     end
 
-    if role.empty? || !self.class::ALLOWED_ROLES.include?(role)
+    if role.blank? || !self.class::ALLOWED_ROLES.include?(role)
       flash[:error] = "You must have one of the following roles #{self.class::ALLOWED_ROLES} in order to access this section. Your role: #{role}"
       redirect_to "/"
     end
