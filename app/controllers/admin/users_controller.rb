8a13a7ff47b29cf4fb46925d51233861c092fe04
Scott Stevenson <sstevenson@credera.com>
2016-12-15 11:41:14 -0600

    Requesting API token for user management from Auth0. This fixes the expired token issue.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,9 @@
 require "auth0"
+require "uri"
+require "net/http"
 
 class Admin::UsersController < Admin::BaseController
+
   def index
     auth0 = get_auth0_client
     @users = auth0.get_users
@@ -27,11 +30,32 @@ class Admin::UsersController < Admin::BaseController
   def get_auth0_client
     return Auth0Client.new(
       :client_id => Rails.application.secrets.auth0_client_id,
-      :token => Rails.application.secrets.auth0_api_token,
+      :token => get_token,
       :domain => Rails.application.config.auth0_endpoint,
       :protocols => 'v2',
       :api_version => 2
     )
   end
 
+  def get_token
+    Rails.cache.fetch('auth0_token', expires_in: 12.hours) do
+      url = URI("https://#{Rails.application.config.auth0_endpoint}/oauth/token")
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      request = Net::HTTP::Post.new(url)
+      request["content-type"] = 'application/json'
+      request.body = "{\"client_id\":\"#{Rails.application.secrets.auth0_client_id}\",\"client_secret\":\"#{Rails.application.secrets.auth0_client_secret}\",\"audience\":\"https://#{Rails.application.config.auth0_endpoint}/api/v2/\",\"grant_type\":\"client_credentials\"}"
+
+      response = http.request(request)
+      Rails.logger.info "Auth0 token request response code: #{response.code}"
+      if response.code != '200'
+        raise "Failed to retrieve token from Auth0"
+      end
+      body = JSON.parse(response.read_body)
+      token = body["access_token"]
+      return token
+    end
+  end
 end
