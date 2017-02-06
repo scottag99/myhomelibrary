71cd23be1d07fa8e9c54c41a6c118409566d197a
Scott Stevenson <sstevenson@credera.com>
2017-02-06 15:40:37 -0600

    Issue #50 Adding sponsored wish list count to admin dashboard

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,5 +1,5 @@
 class Admin::HomeController < Admin::BaseController
   def index
-    @data = {:wishlists => Wishlist.count, :donations => Donation.sum(:amount)}
+    @data = {:wishlists => Wishlist.count, :donations => Donation.sum(:amount), :sponsored_wishlists => Wishlist.includes(:donations).where.not(donations: {id: nil}).count()}
   end
 end
