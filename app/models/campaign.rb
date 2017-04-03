3139077b2fe997fa9c59328db13f79ec4a63fdcd
Scott Stevenson <sstevenson@credera.com>
2017-04-03 13:26:35 -0500

    Fixes campaign delete issue. Cleans up modal quirk with chrome and query.

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,6 +1,6 @@
 class Campaign < ApplicationRecord
   belongs_to :organization
   has_many :wishlists, :dependent => :destroy
-  has_many :campaign_catalogs
+  has_many :campaign_catalogs, :dependent => :delete_all
   has_many :catalogs, through: :campaign_catalogs
 end
