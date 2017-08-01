1c59dc7d20512876f6b179893182e5666bd4a82b
Adam Bergen <abergen@fanreact.com>
2017-08-01 15:15:24 -0500

    styling, pages built, continuing styling

---
+++ app/controllers/sponsors_page_controller.rb
@@ -0,0 +1,2 @@
+class SponsorsPageController < CmsController
+end
---
+++ app/models/sponsors_page.rb
@@ -0,0 +1,6 @@
+class SponsorsPage < Obj
+  attribute :title, :string
+  attribute :body, :widgetlist
+  attribute :child_order, :referencelist
+  attribute :tags, :stringlist
+end
