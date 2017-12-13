be0e78e66eb93bfc5ebb8a7144b594bf67fa698d
Scott Stevenson <sstevenson@stalwartmc.com>
2017-12-13 13:23:26 -0600

    Adding 'Our Schools' page link Moved navbar code to partial for easier re-use Began refactor of scrivito pages to make it more editable by BBHLF

--- app/models/about_page.rb
+++ app/models/about_page.rb
@@ -1,6 +1,8 @@
 class AboutPage < Obj
   attribute :title, :string
+  attribute :subtitle, :string
   attribute :body, :widgetlist
+  attribute :header_class, :string
   attribute :child_order, :referencelist
   attribute :tags, :stringlist
 end
