ddf96c20affddf416103c261c7870c0a8086fbf4
Scott Stevenson <sstevenson@stalwartmc.com>
2018-10-01 16:40:13 -0500

    Show message when delete fails.

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -11,5 +11,9 @@ protected
         errors.add(:base, "There are wishlists with books from this catalog. Deleting will make the system unstable.")
         throw :abort
       end
+      unless campaign_catalogs.size == 0
+        errors.add(:base, "There are campaigns with this catalog assigned. Deleting will make the system unstable.")
+        throw :abort
+      end
     end
 end
